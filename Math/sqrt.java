4b128bce7e6a6de9d816bafd9e9cdaeb41822bc5
wonderfly <shinuo.nankai@gmail.com>
2015-04-29 16:24:28 -0400

    Update sqrt.java

--- Math/sqrt.java
+++ Math/sqrt.java
@@ -27,3 +27,30 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    public int mySqrt(int x) {
+        if(x<0)
+            return -1;
+        if(x==0)
+            return 0;
+        if(x==1)
+            return 1;
+        int lo=1; int hi=x;
+        while(lo+1<hi)
+        {
+            int mid=(lo+hi)/2;
+            if(mid<x/mid)
+                lo=mid;
+            else
+                hi=mid;
+        }
+        //if(lo<x/lo && hi>x/hi)
+        if(hi==x/hi)
+            return hi;
+        else
+            return lo;
+        
+    }
+}
