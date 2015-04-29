644ba5106870a76895375cb6385813a6f7f26ee1
wonderfly <shinuo.nankai@gmail.com>
2015-04-29 16:15:16 -0400

    Create sqrt.java

---
+++ Math/sqrt.java
@@ -0,0 +1,29 @@
+//binary search
+//pay attention to integer overflow. check only mid<x/mid;
+//not mid*mid<x;
+
+
+public class Solution {
+    public int mySqrt(int x) {
+        if(x<0)
+            return -1;
+        if(x==0)
+            return 0;
+        if(x==1)
+            return 1;
+        int lo=1; int hi=x;
+        while(lo<=hi)
+        {
+            int mid=(lo+hi)/2;
+            if(mid==x/mid)
+                return mid;
+            else if(mid<x/mid)
+                lo=mid+1;
+            else
+                hi=mid-1;
+        }
+        
+        return hi;
+        
+    }
+}
