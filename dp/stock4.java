e8700833f3ce592da65dc500607d6262183bda4c
wonderfly <shinuo.nankai@gmail.com>
2015-06-21 16:26:20 -0700

    Update stock4.java

--- dp/stock4.java
+++ dp/stock4.java
@@ -1,1 +1,4 @@
 http://blog.csdn.net/fightforyourdream/article/details/14503469
+
+https://leetcode.com/discuss/38150/simple-dp-8ms-solution-for-best-time-to-buy-and-sell-stock-iii
+https://leetcode.com/discuss/33477/12ms-dp-solution-hopefully-easier-to-understand
