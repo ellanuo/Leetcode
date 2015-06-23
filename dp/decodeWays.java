f729e6c6cbc47c797728707bcfa47d4a21af6a8e
wonderfly <shinuo.nankai@gmail.com>
2015-06-22 17:41:28 -0700

    Update decodeWays.java

--- dp/decodeWays.java
+++ dp/decodeWays.java
@@ -54,3 +54,33 @@ public class Solution {
         return dp[len-1];
     }
 }
+
+
+//better  dp 
+public class Solution {
+    public int numDecodings(String s) {
+       if(s==null ||s.length()==0)  
+            return 0;
+        
+        int [] dp =new int[s.length()+1];
+        dp[0]=1;
+        if(s.charAt(0)=='0')
+            return 0;
+        
+        dp[1]=1;
+        
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)>='1'&&s.charAt(i)<='9')
+                dp[i+1]+=dp[i];
+            
+            if(s.charAt(i-1)=='1' || (s.charAt(i-1)=='2'&& s.charAt(i)>='0'&&s.charAt(i)<='6'))
+                dp[i+1]+=dp[i-1];
+            
+            
+                
+        }
+        
+        return dp[s.length()];
+    }
+}
