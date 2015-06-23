25d484f6bb439c60a9a5b5cae5c72dbb880ebb1a
wonderfly <shinuo.nankai@gmail.com>
2015-06-22 17:47:46 -0700

    Update decodeWays.java

--- dp/decodeWays.java
+++ dp/decodeWays.java
@@ -84,3 +84,40 @@ public class Solution {
         return dp[s.length()];
     }
 }
+
+//constanct space, instead of dp array
+public class Solution {
+    public int numDecodings(String s) {
+       if(s==null ||s.length()==0)  
+            return 0;
+        
+       // int [] dp =new int[s.length()+1];
+        //dp[0]=1;
+        int dp=1;
+        if(s.charAt(0)=='0')
+            return 0;
+        
+        //dp[1]=1;
+        int dp1=1;
+        for(int i=1;i<s.length();i++)
+        {
+            int dp2=0;
+            if(s.charAt(i)>='1'&&s.charAt(i)<='9')
+                //dp[i+1]+=dp[i];
+                dp2+=dp1;
+            
+            if(s.charAt(i-1)=='1' || (s.charAt(i-1)=='2'&& s.charAt(i)>='0'&&s.charAt(i)<='6'))
+                //dp[i+1]+=dp[i-1];
+                dp2+=dp;
+                
+            
+            dp=dp1;
+            dp1=dp2;
+            
+                
+        }
+        
+        //return dp[s.length()];
+        return dp1;
+    }
+}
