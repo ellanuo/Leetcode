e8bbdabc1d5f85985055f973040dd995e7f1b90a
wonderfly <shinuo.nankai@gmail.com>
2015-06-23 13:25:12 -0700

    Update OneEditDistance.java

--- String/OneEditDistance.java
+++ String/OneEditDistance.java
@@ -20,3 +20,26 @@ public boolean isOneEditDistance(String s, String t) {
    while (i < m && s.charAt(i) == t.charAt(i + shift)) i++;   //insert or modify
    return i == m;
 }
+
+bool edit(string s1, string s2) {
+    if(s2.size() < s1.size()) {
+        return edit(s2, s1);
+    }        
+    if(s1.size() + 1 < s2.size()) {
+        return false;
+    }
+
+    int i = 0;
+    while(i < s1.size()) {
+        if(s1[i] != s2[i]) {
+            if(s1.size() == s2.size()) {
+                return s1.substr(i+1) == s2.substr(i+1);
+            }else {
+                return s1.substr(i) == s2.substr(i+1);
+            }
+        }
+        i ++;
+    }
+    # this is for the last char
+    return s1.size() != s2.size();
+}
