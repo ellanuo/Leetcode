798565989ce29bf75af55179d93728e56b3da67a
wonderfly <shinuo.nankai@gmail.com>
2015-07-09 13:48:18 -0700

    Update singleNumber.java

--- bitManipulation/singleNumber.java
+++ bitManipulation/singleNumber.java
@@ -16,3 +16,21 @@ public class Solution {
         return num;
     }
 }
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i=0; i<nums.length;i++)
+        {
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i],1);
+            else
+            {
+                map.remove(nums[i]);
+            }
+        }
+        //if(!map.isEmpty())
+            return map.keySet().iterator().next();
+    }
+}
