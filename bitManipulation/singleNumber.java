b930538a64c28249fc8b4100b7f3c453e7477ee6
wonderfly <shinuo.nankai@gmail.com>
2015-07-09 13:36:11 -0700

    Create singleNumber.java

---
+++ bitManipulation/singleNumber.java
@@ -0,0 +1,18 @@
+/* Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? */
+
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        
+        int num=0;
+        for(int i=0;i<nums.length;i++)
+            num^=nums[i];
+        
+        return num;
+    }
+}
