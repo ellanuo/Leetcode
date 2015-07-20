726f964232c34dd14a5d383c0d3ea31b590a3844
nshihome <nshi.ncsu@gmail.com>
2015-07-20 16:09:19 -0700

    Create search2DMatrix.java

---
+++ binarySearch/search2DMatrix.java
@@ -0,0 +1,42 @@
+/* Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+Given target = 3, return true. */
+
+
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m=matrix.length;
+        int n=matrix[0].length;
+        
+        int i=0;
+        int j=n-1;
+        
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target)
+                return true;
+            
+            else if(target>matrix[i][j])
+                i++;
+            
+            else
+                j--;
+        }
+        
+        return false;
+        
+    }
+}
