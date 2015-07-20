16579c9c4332edb7e3477bb0646c7345285cf74e
nshihome <nshi.ncsu@gmail.com>
2015-07-20 16:24:41 -0700

    Update search2DMatrix.java

--- binarySearch/search2DMatrix.java
+++ binarySearch/search2DMatrix.java
@@ -40,3 +40,35 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m=matrix.length;
+        int n=matrix[0].length;
+        
+        int start=0;
+        int end=m*n-1;
+        
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            int i=mid/n;
+            int j=mid%n;
+            
+            if(matrix[i][j]==target)
+                return true;
+            
+            else if(matrix[i][j]<target)
+                start=mid+1;
+            
+            else
+                end=mid-1;
+        }
+
+        return false;
+        
+        
+    }
+}
