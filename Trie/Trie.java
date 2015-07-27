6494ec06767110c518979a28ac98590fb0277e02
wonderfly <shinuo.nankai@gmail.com>
2015-07-26 17:11:45 -0700

    Update Trie.java

--- Trie/Trie.java
+++ Trie/Trie.java
@@ -73,3 +73,92 @@ public class Trie {
 // Trie trie = new Trie();
 // trie.insert("somestring");
 // trie.search("key");
+
+
+class TrieNode {
+    // Initialize your data structure here.
+    boolean isLeaf;
+    HashMap<Character, TrieNode> map;
+    public TrieNode()
+    {
+        isLeaf=false;
+        map=new HashMap<Character, TrieNode>();
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode p =root;
+        for(int i=0; i<word.length();i++)
+        {
+            if(!p.map.containsKey(word.charAt(i)))
+            {
+                p.map.put(word.charAt(i),new TrieNode());
+            }
+            
+            p=p.map.get(word.charAt(i));
+        }
+        p.isLeaf=true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+      /*  TrieNode p=root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(!p.map.containsKey(word.charAt(i)))
+                return false;
+
+            p=p.map.get(word.charAt(i));
+        }
+        return p.isLeaf; */
+        TrieNode p= getLastNode(word);
+        return p!=null && p.isLeaf;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+      /*   TrieNode p=root;
+        for(int i=0;i<prefix.length();i++)
+        {
+            if(!p.map.containsKey(prefix.charAt(i)))
+                return false;
+          
+          
+            p=p.map.get(prefix.charAt(i));
+        }
+        return true; */
+        
+        return getLastNode(prefix)!=null;
+        
+    }
+    
+    public TrieNode getLastNode(String word)
+    {
+        TrieNode p=root;
+        for(int i=0;i<word.length();i++)
+        {
+            char c=word.charAt(i);
+            if(!p.map.containsKey(c))
+                break;
+            
+            p=p.map.get(c);
+            if(i==word.length()-1)
+                return p;
+        }
+        return null;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
